19377ab13f382ff238b6f51e8a473c88c62f32c8
Reese Pollard <rpollard@gmail.com>
2022-09-27 15:32:21 -0700

    Add cypress for e2e testing.

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
+require('dotenv').config()
 const process = require('process')
 const app = express()
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
+console.log('IS THIS WORKING?', process.env)
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
