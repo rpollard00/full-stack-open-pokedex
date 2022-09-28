fed5216e6458e92086ea860cb7b90ae4a0dcc580
Reese Pollard <rpollard@gmail.com>
2022-09-28 12:53:56 -0700

    11.11 fly.io health check

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
-console.log('IS THIS WORKING?', process.env)
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log('server started on port 5000')
+  console.log(`server started on port ${PORT}`)
+})
+
+app.get('/health', (req, res) => {
+  console.log('health check')
+  res.send('ok')
 })
